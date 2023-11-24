ae40af5d697a68f881f6c5ac6366009524e90390
Ola Al Jarrah <ola.aljarrah20@gmail.com>
2023-11-24 14:20:20 -0500

    updated paragraph

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,14 +4,14 @@ const AboutMe = () => {
     return (
         <div>
             <p> <br></br>
-                Hi 😊 I am Ola, currently in my final year at Sheridan College, pursuing an Advanced Diploma in Software Development and Network Engineering.<br /><br />
+                Hi, I am Ola, currently in my final term at Sheridan College, pursuing an Advanced Diploma in Software Development and Network Engineering.<br /><br />
                 Throughout my academic journey, I have honed a multitude of technical and programming skills.<br /><br />
                 Additionally, I have benefited from two co-op work experiences that provided me with practical, hands-on knowledge.<br />
-                My first co-op position was in Executive IT Support, while my second was in Data Engineering—a role I thoroughly enjoyed.<br /><br />
+                My first co-op position was in Executive IT Support, while my second was in Data Engineering, which I thoroughly enjoyed.<br /><br />
                 My enthusiasm for data engineering led me to further educate myself in this domain.
                 I completed courses such as "Data Engineering Essentials" authorized by IBM through Coursera.<br />
-                Alongside, I deepened my understanding of React.js and took the initiative to learn Python.<br /><br />
-                I am innately curious and continuously seek opportunities to learn about new technologies and expand my skill set.
+                I also deepened my understanding of React.js and took the initiative to learn Python.<br /><br />
+                I am innately curious and I am continuously seeking opportunities to learn about new technologies to expand my skill set.
             </p>
 
 
