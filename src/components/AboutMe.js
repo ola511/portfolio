52e881add0087e6c80b78be1620948ac216e5d02
Ola Al Jarrah <ola.aljarrah20@gmail.com>
2024-03-27 13:46:27 -0400

    Update portfolio text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,12 +4,12 @@ const AboutMe = () => {
     return (
         <div>
             <p> <br></br>
-                Hi, I am Ola, currently in my final term at Sheridan College, pursuing an Advanced Diploma in Computer Systems Technology-Software Development and Network Engineering.<br /><br />
+               Hi, I'm Ola. Currently, I'm in my third work term as a Data Engineer at Skill Squirrel. I pursued an Advanced Diploma in Computer Systems Technology - Software Development and Network Engineering at Sheridan College for three years. I am expected to graduate by April 2024.<br /><br />
                 Throughout my academic journey, I have honed a multitude of technical and programming skills.<br /><br />
                 Additionally, I have benefited from two co-op work experiences that provided me with practical, hands-on knowledge.<br />
                 My first co-op position was in Executive IT Support, while my second was in Data Engineering, which I thoroughly enjoyed.<br /><br />
                 My enthusiasm for data engineering led me to further educate myself in this domain.
-                I completed courses such as "Data Engineering Essentials" authorized by IBM through Coursera.<br />
+                I completed courses such as "Data Engineering Essentials" authorized by IBM through Coursera https://www.credly.com/go/aDvVO9Aj.<br />
                 I also deepened my understanding of React.js and took the initiative to learn Python.<br /><br />
                 I am innately curious and I am continuously seeking opportunities to learn about new technologies to expand my skill set.
             </p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import '../App.css';  // Adjust the path accordingly
+import '../App.css'; // Adjust the path accordingly
+import OlaPic from './imgs/OlaPic.jpg';
 
 const Header = () => {
     return (
         <header>
             <div className="text-center">
-            <h1>Ola Al Jarrah</h1>
+                <h1>Ola Al Jarrah</h1>
             </div>
             <div className="image-container">
-                <img src="/OlaPic.jpg" alt="Profile of Ola" width="80" />
+                <img
+                   src={OlaPic}
+                    alt="Profile of Ola"
+                    className="rounded-image"
+                />
             </div>
-
         </header>
     );
-}
+};
 
 export default Header;
